92fb811458f188a71c3148ed4bf070c962441be6
Kazu122 <75375997+Kazu122@users.noreply.github.com>
2022-07-23 17:48:02 +0900

    コメントアウト

--- webapp/sql/tenant/10_schema.sql
+++ webapp/sql/tenant/10_schema.sql
@@ -31,5 +31,5 @@ CREATE TABLE player_score (
   updated_at BIGINT NOT NULL
 );
 
-CREATE INDEX 'competition_created_at_idx' on competition(created_at);
-CREATE INDEX 'player_score_complex_idx' on player_score(tenant_id, player_id, competition_id);
+-- CREATE INDEX 'competition_created_at_idx' on competition(created_at);
+-- CREATE INDEX 'player_score_complex_idx' on player_score(tenant_id, player_id, competition_id);
