7b9bc8eb33736d7620c46af55579d42b434c96aa
Kazu122 <75375997+Kazu122@users.noreply.github.com>
2022-07-23 16:00:26 +0900

    player_scoreにindexを追加

--- webapp/sql/tenant/10_schema.sql
+++ webapp/sql/tenant/10_schema.sql
@@ -32,3 +32,4 @@ CREATE TABLE player_score (
 );
 
 CREATE INDEX 'competition_created_at_idx' on competition(created_at);
+CREATE INDEX 'player_score_complex_idx' on player_score(tenant_id, player_id, competition_id);
