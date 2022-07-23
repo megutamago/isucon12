7372ded365529ad2ca04c68752a31eaaea38cea0
Kazu122 <75375997+Kazu122@users.noreply.github.com>
2022-07-23 16:08:32 +0900

    player_scoreのrow_numにindexを追加

--- webapp/sql/tenant/add_index.sql
+++ webapp/sql/tenant/add_index.sql
@@ -1,3 +1,4 @@
 
 CREATE INDEX 'competition_created_at_idx' on competition(created_at);
 CREATE INDEX 'player_score_complex_idx' on player_score(tenant_id, player_id, competition_id);
+CREATE INDEX 'player_score_row_num_idx' on player_score(row_num);
