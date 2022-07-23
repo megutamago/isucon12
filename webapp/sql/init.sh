98e330ee04ac7a02673a53955975dd5ca6ef8321
Kazu122 <75375997+Kazu122@users.noreply.github.com>
2022-07-23 15:34:32 +0900

    index追加用のスクリプトを修正

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -21,8 +21,8 @@ rm -f ../tenant_db/*.db
 cp -r ../../initial_data/*.db ../tenant_db/
 
 # indexを付与
-dbArray=`find ../tenant_db -name *.db`
-for db in $dbArray
+dbArray=`find ../../tenant_db -name *.db`
+for db in $dbArray;
 do
-	sqlite3 db < ./add_index.sql
+	sqlite3 db < ./add_index.sql;
 done
